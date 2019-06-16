84ca4f4b74283388ba987475057b509bd60541c1
Rei Ishinabe <rishinabe@gmail.com>
2019-06-16 22:20:42 +0900

    added attribute to user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
   include Pundit
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
 
   # Pundit: white-list approach.
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
@@ -13,6 +15,18 @@ class ApplicationController < ActionController::Base
   #   flash[:alert] = "You are not authorized to perform this action."
   #   redirect_to(root_path)
   # end
+
+
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :image, :is_public])
+  end
+end
+
 
   private
 
