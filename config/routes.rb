d36dad84fd8a3464ba6977ca0ee8241f62b1ffb7
Rei Ishinabe <rishinabe@gmail.com>
2019-06-16 22:09:08 +0900

    set up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :gyms, only [:index, :show, :new, :create, :edit, :update]
+  resources :logs, only [:index, :show, :new, :create, :edit, :update]
 end
