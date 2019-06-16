b5061f424146a1ebb3a94cbb9d7d2072f451af3d
Rei Ishinabe <rishinabe@gmail.com>
2019-06-16 22:25:45 +0900

    fixed syntax errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,8 +25,6 @@ class ApplicationController < ActionController::Base
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :image, :is_public])
   end
-end
-
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :gyms, only [:index, :show, :new, :create, :edit, :update]
-  resources :logs, only [:index, :show, :new, :create, :edit, :update]
+  resources :gyms, only: [:index, :show, :new, :create, :edit, :update]
+  resources :logs, only: [:index, :show, :new, :create, :edit, :update]
 end
