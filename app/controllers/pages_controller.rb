cd85dc219f067a573a43116f8a76fc1b627c524f
Rei Ishinabe <rishinabe@gmail.com>
2019-08-09 09:05:08 +0900

    updated home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    if user_signed_in?
+      @routes = policy_scope(Route).where('user_id = ?', current_user.id)
+    end
   end
 end
