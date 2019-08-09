c35d18227401e0c0d9260f502e7605cd4e526e9f
Rei Ishinabe <rishinabe@gmail.com>
2019-08-09 11:42:09 +0900

    updated footer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,6 +16,9 @@ class ApplicationController < ActionController::Base
   #   redirect_to(root_path)
   # end
 
+  def after_sign_in_path_for(resource)
+    root_path
+  end
 
 
   def configure_permitted_parameters
