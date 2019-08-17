7462f54271b598b9daaf03d3b946ef7232a7cf08
Rei Ishinabe <rishinabe@gmail.com>
2019-08-17 19:24:52 +0900

    added visits by gym chart on logs index

--- app/models/log.rb
+++ app/models/log.rb
@@ -2,7 +2,7 @@ class Log < ApplicationRecord
   belongs_to :route
   has_one :grade, through: :route
   has_many :users, through: :routes
-  has_one :gym, through: :routes
+  has_one :gym, through: :route
   has_one :status
   validates :status_id, presence: true
 end
