6404a35df13d25bb0c5772c3aced7c31254c7957
Rei Ishinabe <rishinabe@gmail.com>
2019-08-22 11:58:06 +0900

    updated ranking page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def index
     if current_user.admin
-      @users = policy_scope(User).all
+      @users = policy_scope(User).all.joins(:logs).group('users.id').order('count(user_id) desc')
     else
       @users = policy_scope(User).all.where('is_public = ?', true)
     end
