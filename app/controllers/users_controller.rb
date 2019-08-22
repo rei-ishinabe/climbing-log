df1520660f7d6ef0e0889341dae24a3bde191251
Rei Ishinabe <rishinabe@gmail.com>
2019-08-22 18:38:40 +0900

    updated users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,8 @@ class UsersController < ApplicationController
     if current_user.admin
       @users = policy_scope(User).all.joins(:logs).group('users.id').order('count(user_id) desc')
     else
-      @users = policy_scope(User).all.where('is_public = ?', true)
+      @users = policy_scope(User).all.where('is_public = ?', true).joins(:logs).group('users.id').order('count(user_id) desc')
+      raise
     end
     authorize @users
   end
