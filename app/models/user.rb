6e4bc9a427ee74c47e6b411e68408cea06dc2990
Rei Ishinabe <rishinabe@gmail.com>
2019-08-28 22:36:13 +0900

    fixed a bug on users index page

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,4 @@ class User < ApplicationRecord
   has_many :logs, through: :routes
   has_many :gyms, through: :routes
   validates :email, presence: true, uniqueness: true
-
-  def best_boulder_sent
-    routes.joins(:logs).where('routes.category_id = ? and logs.status_id < ?', 1, 4).order(grade_id: 'DESC').order(sub_grade_id: 'DESC').order(date: 'DESC').first
-  end
-
-  def best_lead_sent
-    routes.joins(:logs).where('routes.category_id = ? and logs.status_id < ?', 2, 4).order(grade_id: 'DESC').order(sub_grade_id: 'DESC').order(date: 'DESC').first
-  end
 end
