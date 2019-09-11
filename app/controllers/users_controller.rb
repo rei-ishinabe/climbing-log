ed7a3659a1f3fb8d51d72aedacab66f6f349a7a3
Rei Ishinabe <rishinabe@gmail.com>
2019-09-11 18:05:04 +0900

    fixed a bug on ranking page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,9 +13,9 @@ class UsersController < ApplicationController
     end
 
     if current_user.admin
-      @users = policy_scope(User).all.joins(:logs).where('logs.date BETWEEN ? AND ? and logs.status_id < ?', @from, @to, 4).group('users.id').order('count(user_id) desc')
+      @users = policy_scope(User).all.joins(:logs).where('logs.date BETWEEN ? AND ? AND logs.status_id < ?', @from, @to, 4).group('users.id').order('count(user_id) desc')
     else
-      @users = policy_scope(User).all.joins(:logs).where('logs.date BETWEEN ? AND ? AND is_public = ?', @from, @to, true).group('users.id').order('count(user_id) desc')
+      @users = policy_scope(User).all.joins(:logs).where('logs.date BETWEEN ? AND ? AND logs.status_id < ? AND is_public = ?', @from, @to, 4, true).group('users.id').order('count(user_id) desc')
     end
     authorize @users
   end
