4ba3929ba7b4702614df32b93ff73166fb7cb76f
Rei Ishinabe <rishinabe@gmail.com>
2019-10-07 18:00:17 +0900

    added hash string to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_20_044700) do
+ActiveRecord::Schema.define(version: 2019_10_07_085201) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -164,6 +164,7 @@ ActiveRecord::Schema.define(version: 2019_09_20_044700) do
     t.string "image"
     t.boolean "is_public", default: false
     t.integer "privacy_setting", default: 0
+    t.string "hash_string"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
